1106929000556c4ccc97115c7ebe46e6403e343a
mitvadesai <hiraldesai7575@gmail.com>
2025-04-02 09:16:41 +0530

    first commit

---
+++ react-exam/src/firebaseConfig.js
@@ -0,0 +1,47 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBwqRoRGxQeDPwCW-26OnFejOjRrmes1iY",
+//   authDomain: "img-upaload.firebaseapp.com",
+//   projectId: "img-upaload",
+//   storageBucket: "img-upaload.firebasestorage.app",
+//   messagingSenderId: "1076416011663",
+//   appId: "1:1076416011663:web:dbb10b01c892b74b32fffa",
+//   measurementId: "G-GN68B52N7V"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export const db = getFirestore(app);
+// export const auth = getAuth(app);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMKCGLNzsYRGHlcIdGkL9kLjy7C2s1gEs",
+  authDomain: "react-exam-40cfa.firebaseapp.com",
+  projectId: "react-exam-40cfa",
+  storageBucket: "react-exam-40cfa.firebasestorage.app",
+  messagingSenderId: "91128004403",
+  appId: "1:91128004403:web:245be3f3f1c40f108e8846",
+  measurementId: "G-WK16NE4XTE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
